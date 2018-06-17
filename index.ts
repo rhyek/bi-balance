9703fbf8e1cad463a7c3096ad09d4667239112a1
Carlos González <carlos.rgn@gmail.com>
2018-06-17 14:26:45 -0600

    better vsc launch/task for dev and better json payload structure

--- index.ts
+++ index.ts
@@ -54,7 +54,7 @@ app.use(async ctx => {
     td => td.textContent,
     await tipoCambioFrame.waitForSelector('table > tbody > tr > td')
   );
-  const exchangeRate = {
+  const exchangeRates = {
     buy: parseFloat(ratesText.match(/Compra: (.+)/)[1]),
     sell: parseFloat(ratesText.match(/Venta: (.+)/)[1]),
   };
@@ -77,42 +77,57 @@ app.use(async ctx => {
   await accountsLink.click();
   await accountsPromise;
 
+  const accounts = [];
+
   const mainFrame = page.frames().find(frame => frame.name() === 'main')!;
-  const accountNumber = await mainFrame.evaluate(
-    a => a.textContent.trim(),
-    await mainFrame.waitForSelector(
-      'table table > tbody > tr:nth-of-type(3) > td:nth-of-type(2) > a'
-    )
-  );
-  const availableRaw = await mainFrame.evaluate(
-    td => td.textContent.trim(),
-    await mainFrame.waitForSelector('table table > tbody > tr:nth-of-type(3) > td:nth-of-type(4)')
-  );
+  const table = await mainFrame.waitForSelector('table table');
+  const accountTRs = await table.$$('table table > tbody > tr[align=right]');
+
+  for (const tr of accountTRs) {
+    const currencyRaw: string = await mainFrame.evaluate(
+      (td: HTMLTableCellElement) => td.textContent,
+      await tr.$('td:nth-of-type(1)')
+    );
+    const currency = currencyRaw.match(/(.+)\./)![1];
+
+    const number: string = await mainFrame.evaluate(
+      (a: HTMLAnchorElement) => a.textContent!.trim(),
+      await tr.$('td:nth-of-type(2) > a')
+    );
+    const availableRaw = await mainFrame.evaluate(
+      (td: HTMLTableCellElement) => td.textContent!.trim(),
+      await tr.$('td:nth-of-type(4)')
+    );
+
+    const rateUsed = currency === 'Q' ? 'sell' : 'buy';
+
+    const Q = parseFloat(availableRaw.replace(/,/g, ''));
+    const $ = parseFloat(new Decimal(Q).div(exchangeRates[rateUsed]).toFixed(2));
+
+    const available = {
+      raw: { Q, $ },
+      formatted: {
+        Q: numeral(Q).format('0,0.00'),
+        $: numeral($).format('0,0.00'),
+      },
+    };
+
+    accounts.push({
+      number,
+      currency,
+      rateUsed,
+      available,
+    });
+  }
 
   await page.close();
-
-  const Q = parseFloat(availableRaw.replace(/,/g, ''));
-  const available = {
-    Q,
-    $: parseFloat(new Decimal(Q).div(exchangeRate.sell).toFixed(2)),
-  };
-  const formatted = {
-    Q: numeral(available.Q).format('0,0.00'),
-    $: numeral(available.$).format('0,0.00'),
-  };
 
   const endTime = new Date();
   const duration = endTime.getTime() - startTime.getTime();
 
   const result = {
-    accounts: [
-      {
-        accountNumber,
-        available,
-        formatted,
-      },
-    ],
-    exchangeRate,
+    accounts,
+    exchangeRates,
     duration,
   };
 
